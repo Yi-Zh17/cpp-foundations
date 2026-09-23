250aa46084f0277ed3bc47c4132ec185cfb462b8
Yi Zhang <y_zh426@outlook.com>
2026-09-23 09:38:25 +0100

    chore: clean up example files

--- src/my_string.cpp
+++
@@ -1,9 +0,0 @@
-#include "foundations/my_string.hpp"
-
-#include <cstring>
-
-namespace foundations {
-
-MyString::MyString(const char* s) : size_(s != nullptr ? std::strlen(s) : 0) {}
-
-}  // namespace foundations
